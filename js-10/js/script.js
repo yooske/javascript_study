819b4640ed269c334d7222a61ead36a6c43c48cd
yooske <yooske0214@gmail.com>
2022-11-02 15:03:48 +0000

    [add] js-10 finish

--- js-10/js/script.js
+++ js-10/js/script.js
@@ -1,8 +1,7 @@
 $(function() {
-  $('#back a').on('click',function(event){
-    $('body, html').animate({
-      scrollTop:0
-    }, 800);
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
     event.preventDefault();
   });
 });
